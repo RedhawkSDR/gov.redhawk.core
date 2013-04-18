5397eb0e3c19d01467c05e472dd8fbab6b49b695
Erik Englund <erik.englund@axiosengineering.com>
2013-04-18 16:58:39 -0400

    ref #608 forwards all selection events also to post selection listeners.  We may want to revisit this when we figure out how it is supposed to be done.

--- plugins/gov.redhawk.ui/src/gov/redhawk/ui/editor/SCAFormEditor.java
+++ plugins/gov.redhawk.ui/src/gov/redhawk/ui/editor/SCAFormEditor.java
@@ -464,6 +464,12 @@ public abstract class SCAFormEditor extends FormEditor implements IEditingDomain
 		public IWorkbenchPart getPart() {
 			return getMultiPageEditor();
 		}
+		
+		@Override
+		protected void handleSelectionChanged(SelectionChangedEvent event) {
+		    super.handleSelectionChanged(event);
+		    super.handlePostSelectionChanged(event);
+		}
 	}
 
 	/**
