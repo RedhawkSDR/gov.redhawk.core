RedhawkSDR/gov.redhawk.core

commit 7eabb4100730eec5759fbbbea451531884644d45

use the NeXtMidas 3.3.1 /msgid= to register message handler for macro and MESSAGE SEND this.msgid inside macro to forward message's to the registered message handler for the macro. also increase CORBARECEIVER poll time to 0.25 secs vs default of 1/80 = 0.0125 secs (since it doesn't do anything in it's process() method

plugins/nxm.redhawk/src/nxm/redhawk/mcr/bulkio2midas.mm
@@ -1,7 +1,6 @@
 STARTMACRO t:corbaargs t:fftargs t:thinargs s:output
-    switch MSGID_TMP msgHandlerID GET DEF=null
     PIPE INIT
-        corbareceiver/MSGID=MAIN/TLL=200/PS=^corbaargs.PIPESIZE FILE=_CORBA_OUT HOST="^corbaargs.HOST" PORT=^corbaargs.PORT &
+        corbareceiver/MSGID=MAIN/TLL=200/PS=^corbaargs.PIPESIZE/POLL=0.25 FILE=_CORBA_OUT HOSgit sT="^corbaargs.HOST" PORT=^corbaargs.PORT &
             RESOURCE=^corbaargs.RESOURCE PORT_NAME=^corbaargs.PORT_NAME IDL="^corbaargs.IDL" FORCE2000=^corbaargs.FORCE2000
         if fftargs isNULL then
             set fftout _CORBA_OUT
@@ -16,17 +15,15 @@ STARTMACRO t:corbaargs t:fftargs t:thinargs s:output
             dispthin/PS=^{thinargs.pipesize}/TL=1 ^fftout ^thinout ^thinargs.refreshrate
         endif
         ! put output pipe into parent macro's PIPE/RAM results table so that PLOT can see it
-        set/parent RAM.^{output} RAM.^{thinout}
+        SET/PARENT RAM.^{output} RAM.^{thinout}
 
     PIPE OFF
 
     ! cleanup entry that we added in parent macro's PIPE/RAM results table
-    remove/parent RAM.^{output}
+    REMOVE/PARENT RAM.^{output}
 ENDMACRO
 
 PROCEDURE processMessage m:msg
-    if msgHandlerID nIsNULL then
-        !say "Forwarding msg: ^msg"
-        message send PARENT.^{msgHandlerID} msg
-    endif
+    ! forward messages (e.g. from corbareceiver) to registered message handler for this macro (requires NeXtMidas 3.3.1+)
+    MESSAGE SEND THIS.MSGID msg
 RETURN

plugins/gov.redhawk.ui.port.nxmplot.rcp/src/gov/redhawk/ui/port/nxmplot/rcp/RcpNxmPlotWidget.java
@@ -1,4 +1,4 @@
-/** 
+/**
  * This file is protected by Copyright. 
  * Please refer to the COPYRIGHT file distributed with this source distribution.
  * 
@@ -31,22 +31,22 @@ import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
 
 /**
- * 
+ *
  */
 public class RcpNxmPlotWidget extends AbstractNxmPlotWidget {
 
 	private NeXtMidasComposite nxmComp;
 	private plot plotCommand;
-	
+
 	private static final String MSG_HANDLER_ID = "MAIN_MSG_HANLDER";
-	
+
 	private Set<String> sources = new HashSet<String>();
 	private boolean initialized;
 
 	public RcpNxmPlotWidget(final Composite parent, int style) {
 		super(parent, style);
 		parent.addDisposeListener(new DisposeListener() {
-			
+
 			@Override
 			public void widgetDisposed(DisposeEvent e) {
 				dispose();
@@ -54,7 +54,7 @@ public class RcpNxmPlotWidget extends AbstractNxmPlotWidget {
 		});
 		setLayout(new FillLayout());
 		nxmComp = new NeXtMidasComposite(this, SWT.None);
-		
+
 		RedhawkNxmUtil.initializeRedhawkOptionTrees();
 		nxmComp.getLocalShell().M.registry.put(MSG_HANDLER_ID, this.plotMessageHandler);
 	}
@@ -64,27 +64,27 @@ public class RcpNxmPlotWidget extends AbstractNxmPlotWidget {
 			throw new IllegalStateException("Plot already initialized.");
 		}
 		this.initialized = true;
-		
+
 		if (plotArgs==null) {
 			plotArgs = "";
 		}
 		if (plotSwitches == null) {
 			plotSwitches = "";
 		}
-		plotCommand = (plot) nxmComp.runCommand("plot/bg" + plotSwitches + " " + plotArgs);
+		plotCommand = (plot) nxmComp.runCommand("PLOT/BG/VERBOSE=FALSE" + plotSwitches + " " + plotArgs);
 		plotCommand.setMessageHandler(plotMessageHandler);
 	}
 
 	@Override
 	public void runHeadlessCommand(String command) {
-		nxmComp.runCommand(command + " /MSGID_TMP=" + MSG_HANDLER_ID, false);
+		nxmComp.runCommand(command + " /MSGID=" + MSG_HANDLER_ID, false);
 	}
 
 	@Override
 	public void runClientCommand(String command) {
-		runHeadlessCommand(command); 
+		runHeadlessCommand(command);
     }
-	
+
 	@Override
 	public void dispose() {
 		String[] sourcesCopy = Arrays.copyOf(sources.toArray(new String[0]), sources.size());
@@ -104,20 +104,20 @@ public class RcpNxmPlotWidget extends AbstractNxmPlotWidget {
 	public void addSource(String sourcePipeId) {
 		addSource(sourcePipeId, null);
     }
-	
+
 	@Override
 	public void addSource(String sourcePipeId, String pipeQualifiers) {
-		nxmComp.runCommand("sendto " + plotCommand.id + " OPENFILE " + sourcePipeId + (pipeQualifiers == null ? "" : pipeQualifiers));
+		nxmComp.runCommand("SENDTO " + plotCommand.id + " OPENFILE " + sourcePipeId + (pipeQualifiers == null ? "" : pipeQualifiers));
 		this.sources.add(sourcePipeId);
     }
-	
+
 	public Set<String> getSources() {
 		return Collections.unmodifiableSet(this.sources);
 	}
 
 	@Override
 	public void removeSource(String sourcePipeId) {
-		nxmComp.runCommand("sendto " + plotCommand.id + " CLOSEFILE " + sourcePipeId);
+		nxmComp.runCommand("SENDTO " + plotCommand.id + " CLOSEFILE " + sourcePipeId);
 		this.sources.remove(sourcePipeId);
     }
 
@@ -128,21 +128,21 @@ public class RcpNxmPlotWidget extends AbstractNxmPlotWidget {
 		// XXX We need to rethink this for RAP support
 		String tempResName = createUniqueResName();
 		nxmComp.getLocalShell().M.results.put(tempResName, data); //to pass object reference for data= in the SENDTO command
-		nxmComp.runCommand("sendto " + plotCommand.id + " "+ msgName + " " + tempResName + " INFO=" + info);
+		nxmComp.runCommand("SENDTO " + plotCommand.id + " "+ msgName + " " + tempResName + " INFO=" + info);
 		nxmComp.getLocalShell().M.results.remove(tempResName); // cleanup
 	}
-	
+
 	private static AtomicInteger uniqueCounter = new AtomicInteger();
-	
+
 	private static String createUniqueResName() {
 		return "_TEMPRES_" + uniqueCounter.incrementAndGet();
 	}
-	
+
 	//This method available only in the RCP plot widget
 	public plot getPlot() {
 		return this.plotCommand;
 	}
-	
+
 	@Override
 	public String addDataFeature(Number xStart, Number xEnd, String color) {
 		String featureId = AbstractNxmPlotWidget.createUniqueName(false);
@@ -152,7 +152,7 @@ public class RcpNxmPlotWidget extends AbstractNxmPlotWidget {
 		this.runClientCommand(cmd);
 		return featureId;
 	}
-	
+
 	@Override
 	public String addDragboxFeature(Number xmin, Number ymin, Number xmax, Number ymax, String color) {
 		String featureId = AbstractNxmPlotWidget.createUniqueName(false);
@@ -165,17 +165,17 @@ public class RcpNxmPlotWidget extends AbstractNxmPlotWidget {
 		this.runClientCommand(command);
 		return featureId;
 	}
-	
+
 	@Override
 	public void removeFeature(String featureId) {
-		final String command = "invoke junk reg." + plotCommand.id + ".removeFeature(\"" + featureId + "\")";
+		final String command = "INVOKE junk reg." + plotCommand.id + ".removeFeature(\"" + featureId + "\")";
 		this.runClientCommand(command);
 	}
 
 	@Override
     public void configurePlot(Map<String, String> configuration) {
 	    for (Map.Entry<String, String> entry: configuration.entrySet()) {
-	    	nxmComp.runCommand("set " + "REG." + plotCommand.id + "." + entry.getKey() + " " + entry.getValue());
+	    	nxmComp.runCommand("SET " + "REG." + plotCommand.id + "." + entry.getKey() + " " + entry.getValue());
 	    }
     }
 

plugins/nxm.redhawk/META-INF/MANIFEST.MF
@@ -1,7 +1,7 @@
 Manifest-Version: 1.0
 Bundle-ManifestVersion: 2
 Implementation-Version: 3.2.0
-Bundle-Name: Redhawk Nxm Option Tree Plug-in
+Bundle-Name: REDHAWK NeXtMidas Option Tree Plug-in
 Bundle-SymbolicName: nxm.redhawk;singleton:=true
 Bundle-Version: 8.1.0.qualifier
 Bundle-Vendor: REDHAWK
@@ -9,8 +9,8 @@ Bundle-Localization: plugin
 Require-Bundle: mil.jpeojtrs.sca.cf;bundle-version="2.2.2",
  idl.bulkio;bundle-version="1.7.0",
  mil.jpeojtrs.sca.util;bundle-version="3.0.0",
- nxm.sys;bundle-version="3.0.1";visibility:=reexport,
- nxm.dsp;bundle-version="3.2.0"
+ nxm.sys;bundle-version="3.3.1";visibility:=reexport,
+ nxm.dsp;bundle-version="3.3.1"
 DynamicImport-Package: *
 Bundle-ActivationPolicy: lazy
 Eclipse-RegisterBuddy: nxm.sys

commit be98da0fdcb1adfe7e0173f801972a2a86aa9f86

fix typo caused by commit 7eabb4100730eec5759fbbbea451531884644d45 in call to CORBARECEIVER for passing HOST= argument

plugins/nxm.redhawk/src/nxm/redhawk/mcr/bulkio2midas.mm
@@ -1,6 +1,6 @@
 STARTMACRO t:corbaargs t:fftargs t:thinargs s:output
     PIPE INIT
-        corbareceiver/MSGID=MAIN/TLL=200/PS=^corbaargs.PIPESIZE/POLL=0.25 FILE=_CORBA_OUT HOSgit sT="^corbaargs.HOST" PORT=^corbaargs.PORT &
+        corbareceiver/MSGID=MAIN/TLL=200/PS=^corbaargs.PIPESIZE/POLL=0.25 FILE=_CORBA_OUT HOST="^corbaargs.HOST" PORT=^corbaargs.PORT &
             RESOURCE=^corbaargs.RESOURCE PORT_NAME=^corbaargs.PORT_NAME IDL="^corbaargs.IDL" FORCE2000=^corbaargs.FORCE2000
         if fftargs isNULL then
             set fftout _CORBA_OUT