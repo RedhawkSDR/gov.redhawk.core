eff5441a294439179c0a124181d341702fa52e83
Erik Englund <erik.englund@axiosengineering.com>
2013-04-29 17:10:10 -0400

    refs #685 Add workspace attribute for launching waveforms.

--- plugins/gov.redhawk.sca.launch/src/gov/redhawk/sca/launch/ScaLaunchConfigurationConstants.java
+++ plugins/gov.redhawk.sca.launch/src/gov/redhawk/sca/launch/ScaLaunchConfigurationConstants.java
@@ -26,6 +26,7 @@ public interface ScaLaunchConfigurationConstants { // SUPPRESS CHECKSTYLE Ignore
 	 * Attribute to indicate the path to the profile xml
 	 */
 	public static final String ATT_PROFILE = ScaLaunchActivator.ID + ".profile";
+	public static final String ATT_WORKSPACE = ScaLaunchActivator.ID + ".workspace";
 	/**
 	 * Attribute to indicate the resource should be started after launching
 	 */
