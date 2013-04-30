f66634e689b455ce7ca3c88d032082334fd09db4
Neon Ngo <ntn@techma.com>
2013-04-30 16:47:51 -0400

    refs #693 suppress verbose output for OPTION, PATH, and DEBUG NeXtMidas commands

--- plugins/nxm.redhawk/src/nxm/redhawk/lib/RedhawkNxmUtil.java
+++ plugins/nxm.redhawk/src/nxm/redhawk/lib/RedhawkNxmUtil.java
@@ -5,22 +5,22 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import nxm.sys.lib.NeXtMidas;
 
 public final class RedhawkNxmUtil {
-	
+
 	private static final AtomicBoolean added = new AtomicBoolean(false);
 
 	private RedhawkNxmUtil() {
 
 	}
-	
+
 	public static void initializeRedhawkOptionTrees() {
 		if (!added.compareAndSet(false, true)) {
 			return;
 		}
 		NeXtMidas instance = NeXtMidas.getGlobalInstance();
-		instance.runCommand("OPTION REDHAWK CLASSPATH");
-		instance.runCommand("OPTION DSP CLASSPATH");
-		instance.runCommand("PATH ADD DSP REDHAWK");
-		instance.runCommand("DEBUG ON TRACE");
+		instance.runCommand("OPTION/VERBOSE=false REDHAWK CLASSPATH");
+		instance.runCommand("OPTION/VERBOSE=false DSP CLASSPATH");
+		instance.runCommand("PATH/VERBOSE=false ADD DSP REDHAWK");
+		instance.runCommand("DEBUG/VERBOSE=false ON TRACE");
 	}
 
 }
