f815fcef6315710926c5e26fed693487391e8aa1
Neon Ngo <ntn@techma.com>
2013-06-04 13:57:51 -0400

    refs #836 fix API baseline warning by adding @since 9.1 to new method and checkstyle warnings for unused imports

--- plugins/gov.redhawk.sca.ui/src/gov/redhawk/sca/ui/compatibility/CompatibilityUtil.java
+++ plugins/gov.redhawk.sca.ui/src/gov/redhawk/sca/ui/compatibility/CompatibilityUtil.java
@@ -1,10 +1,10 @@
-/** 
- * This file is protected by Copyright. 
+/**
+ * This file is protected by Copyright.
  * Please refer to the COPYRIGHT file distributed with this source distribution.
- * 
+ *
  * This file is part of REDHAWK IDE.
- * 
- * All rights reserved.  This program and the accompanying materials are made available under 
+ *
+ * All rights reserved.  This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html.
  *
@@ -17,8 +17,6 @@ import org.eclipse.jface.viewers.ComboViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.ui.IWorkbenchPartSite;
-import org.eclipse.ui.internal.PartSite;
 
 /**
  * @since 9.0
@@ -28,15 +26,18 @@ public class CompatibilityUtil {
 	private CompatibilityUtil() {
 		//Prevent instantiation
 	}
-	
+
 	public static void setFontDataStyle(FontData fontData, int style) {
 		throw new UnsupportedOperationException();
 	}
-	
+
 	public static void disableComboWheelScrollSelect(ComboViewer viewer) {
 		throw new UnsupportedOperationException();
 	}
-	
+
+	/**
+     * @since 9.1
+     */
 	public static Principal getUserPrincipal(Display display) {
 		// Throw exception if RAP implementation not present. There is no RCP implementation, so just return null
 		if (SWT.getPlatform().startsWith("rap")) {
