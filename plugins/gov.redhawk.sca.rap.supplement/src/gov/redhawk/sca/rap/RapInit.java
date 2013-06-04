4d6ff140254e906382f11cc77186ec7f1a862fd4
Neon Ngo <ntn@techma.com>
2013-06-04 13:59:55 -0400

    refs #836 bump minor version to 1.1 since there is new API added in gov.redhawk.sca.rap.supplement plug-in +removed unused imports

--- plugins/gov.redhawk.sca.rap.supplement/src/gov/redhawk/sca/rap/RapInit.java
+++ plugins/gov.redhawk.sca.rap.supplement/src/gov/redhawk/sca/rap/RapInit.java
@@ -1,21 +1,20 @@
 package gov.redhawk.sca.rap;
 
 import java.security.Principal;
-import java.util.Enumeration;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.eclipse.rwt.RWT;
-import org.eclipse.rwt.service.ISessionStore;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IEditorSite;
 
 public class RapInit {
-	
+
 	/**
-	 * 
-	 * @deprecated Use {@link #getUserPrincipal(Display)} instead.
+	 *
+	 * @deprecated Since 1.1 use {@link #getUserPrincipal(Display)} instead.
 	 */
+	@Deprecated
 	public static void init(final IEditorSite site) {
 		final HttpServletRequest req = RWT.getRequest();
 		RWT.requestThreadExec(new Runnable() {
@@ -34,10 +33,11 @@ public class RapInit {
 					}
 				}
 			}
-			
+
 		});
 	}
-	
+
+	/** @since 1.1 */
 	public static Principal getUserPrincipal(final Display display) {
 		final Principal[] user = new Principal[1];
 		final Boolean[] done = new Boolean[1];
@@ -50,9 +50,9 @@ public class RapInit {
 					user[0] = findUser(display);
 					done[0] = true;
 				}
-				
+
 			});
-			
+
 			while (!done[0]) {
 				if (!display.readAndDispatch()) {
 					display.sleep();
@@ -63,7 +63,7 @@ public class RapInit {
 			return findUser(display);
 		}
 	}
-	
+
 	private static Principal findUser(Display display) {
 		final Boolean[] done = new Boolean[1];
 		done[0] = false;
@@ -76,9 +76,9 @@ public class RapInit {
 				}
 				done[0] = true;
 			}
-			
+
 		});
-		
+
 		while (!done[0]) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
