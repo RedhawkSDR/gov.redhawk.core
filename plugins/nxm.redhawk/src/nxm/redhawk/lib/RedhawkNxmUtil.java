4775174bef9fb67b0472c82e65d9f90cd69145cb
Neon Ngo <ntn@techma.com>
2013-06-27 13:42:34 -0400

    refs #693 - for some reason my commits in revision f66634e6 is no longer on develop (1.9) branch - reapplying to suppress verbose output for NeXtMidas commands executed once on startup

--- plugins/nxm.redhawk/src/nxm/redhawk/lib/RedhawkNxmUtil.java
+++ plugins/nxm.redhawk/src/nxm/redhawk/lib/RedhawkNxmUtil.java
@@ -1,10 +1,10 @@
-/** 
- * This file is protected by Copyright. 
+/**
+ * This file is protected by Copyright.
  * Please refer to the COPYRIGHT file distributed with this source distribution.
  *
  * This file is part of REDHAWK IDE.
- * 
- * All rights reserved.  This program and the accompanying materials are made available under 
+ *
+ * All rights reserved.  This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html.
  *
@@ -21,16 +21,16 @@ public final class RedhawkNxmUtil {
 	private RedhawkNxmUtil() {
 
 	}
-	
+
 	public static void initializeRedhawkOptionTrees() {
 		if (!ADDED.compareAndSet(false, true)) {
 			return;
 		}
 		NeXtMidas instance = NeXtMidas.getGlobalInstance();
-		instance.runCommand("OPTION REDHAWK CLASSPATH");
-		instance.runCommand("OPTION DSP CLASSPATH");
-		instance.runCommand("PATH ADD DSP REDHAWK");
-		instance.runCommand("DEBUG ON TRACE");
+		instance.runCommand("OPTION/VERBOSE=false REDHAWK CLASSPATH");
+		instance.runCommand("OPTION/VERBOSE=false DSP CLASSPATH");
+		instance.runCommand("PATH/VERBOSE=false   ADD DSP REDHAWK");
+		instance.runCommand("DEBUG/VERBOSE=false  ON TRACE");
 	}
 
 }
