94ec131f4f344cfbcf9d588c362bd3260caa30ee
Neon Ngo <ntn@techma.com>
2014-01-29 16:17:04 -0500

    refs #1584 add new Plot Preference Page for global IDE preferences to enable/disable the NeXtMidas PLOT's configure menu using the middle mouse button (MMB or middle-click) (defaults to disabled)

---
+++ plugins/gov.redhawk.ui.port.nxmplot/src/gov/redhawk/ui/port/nxmplot/preferences/PlotPreferenceConstants.java
@@ -0,0 +1,23 @@
+/**
+ * This file is protected by Copyright.
+ * Please refer to the COPYRIGHT file distributed with this source distribution.
+ *
+ * This file is part of REDHAWK IDE.
+ *
+ * All rights reserved.  This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html.
+ */
+package gov.redhawk.ui.port.nxmplot.preferences;
+
+/**
+ * Constant definitions for Port Plot (nxmplot) plug-in preferences
+ */
+public class PlotPreferenceConstants {
+
+	public static final String P_ENABLE_CONFIGURE_MENU_USING_MOUSE = "enablePlotConfigureMenuUsingMouse";
+
+	/** hide constructor as this is a utility class with only static fields/methods. */
+	private PlotPreferenceConstants() {
+	}
+}
---
+++ plugins/gov.redhawk.ui.port.nxmplot/src/gov/redhawk/ui/port/nxmplot/preferences/PlotPreferenceInitializer.java
@@ -0,0 +1,33 @@
+/**
+ * This file is protected by Copyright.
+ * Please refer to the COPYRIGHT file distributed with this source distribution.
+ *
+ * This file is part of REDHAWK IDE.
+ *
+ * All rights reserved.  This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html.
+ */
+package gov.redhawk.ui.port.nxmplot.preferences;
+
+import gov.redhawk.sca.ui.ScaUiPlugin;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.jface.preference.IPreferenceStore;
+
+/**
+ * Class used to initialize default preference values.
+ */
+public class PlotPreferenceInitializer extends AbstractPreferenceInitializer {
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#initializeDefaultPreferences()
+	 */
+	public void initializeDefaultPreferences() {
+		IPreferenceStore store = ScaUiPlugin.getDefault().getPreferenceStore();
+		store.setDefault(PlotPreferenceConstants.P_ENABLE_CONFIGURE_MENU_USING_MOUSE, false);
+	}
+
+}
