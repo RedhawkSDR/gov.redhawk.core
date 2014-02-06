26d918788d83b87c5938fbb2744493d30f5a940f
Mark Leone <mal@axiosengineering.com>
2014-02-06 16:59:39 -0500

    Allow all tuner types

--- plugins/gov.redhawk.frontend.ui/src/gov/redhawk/frontend/ui/FrontEndUIActivator.java
+++ plugins/gov.redhawk.frontend.ui/src/gov/redhawk/frontend/ui/FrontEndUIActivator.java
@@ -45,10 +45,10 @@ public class FrontEndUIActivator extends AbstractUIPlugin {
 	public static List<String> supportedTunerTypes = new ArrayList<String>(Arrays.asList(
 			new String[] {
 				FRONTEND.TUNER_TYPE_RX_DIGITIZER.value
-//				,FRONTEND.TUNER_TYPE_CHANNELIZER.value
-//				,FRONTEND.TUNER_TYPE_DDC.value
-//				,FRONTEND.TUNER_TYPE_RX.value
-//				,FRONTEND.TUNER_TYPE_RX_DIGITIZER_CHANNELIZER.value
+				,FRONTEND.TUNER_TYPE_CHANNELIZER.value
+				,FRONTEND.TUNER_TYPE_DDC.value
+				,FRONTEND.TUNER_TYPE_RX.value
+				,FRONTEND.TUNER_TYPE_RX_DIGITIZER_CHANNELIZER.value
 				}
 		));
 	
