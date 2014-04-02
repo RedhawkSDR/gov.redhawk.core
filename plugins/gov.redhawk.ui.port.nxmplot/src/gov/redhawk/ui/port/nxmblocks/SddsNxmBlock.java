1539961a591e60fe037f28a9bb6b8fc5cb7af8bd
Neon Ngo <ntn@techma.com>
2014-04-02 17:34:49 -0400

    add setter methods for settings and fix applySettings method cleanup formCmdLine method to better handle when byteOrder and the various other settings are null or not set

--- plugins/gov.redhawk.ui.port.nxmplot/src/gov/redhawk/ui/port/nxmblocks/SddsNxmBlock.java
+++ plugins/gov.redhawk.ui.port.nxmplot/src/gov/redhawk/ui/port/nxmblocks/SddsNxmBlock.java
@@ -28,7 +28,7 @@ import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.util.PropertyChangeEvent;
 
 /**
- * SDDS (UDP/Multicast) source NXM block.
+ * SDDS (Multicast) source NXM block.
  * @noreference This class is provisional/beta and is subject to API changes
  * @since 4.4
  */
@@ -64,19 +64,28 @@ public class SddsNxmBlock extends AbstractNxmBlock<sourcenic> {
 		final String outputName = AbstractNxmPlotWidget.createUniqueName(true);
 		putOutputNameMapping(0, streamID, outputName); // save output name mapping
 
-		final StringBuilder switches = new StringBuilder("");
+		final StringBuilder switches = new StringBuilder(256);
+		String mcastAddress = getMcastAddress();
+		if (mcastAddress != null && !mcastAddress.isEmpty()) {
+			switches.append("/MGRP=").append(mcastAddress);
+		}
+		switches.append("/PORT=").append(getPort());
+		switches.append("/VLAN=").append(getVlan());
+		ByteOrder byteOrder = getDataByteOrder();
+		if (byteOrder != null) {
+			switches.append("/BYTEORDER=").append(byteOrder);
+		}
+		String outputFormat = getOutputFormat();
+		if (outputFormat != null && !outputFormat.isEmpty()) {
+			switches.append("/FC=").append(outputFormat);
+		}
 		final int pipeSize = getPipeSize(); // in bytes
 		if (pipeSize > 0) {
 			switches.append("/PS=").append(pipeSize);
 		}
-		ByteOrder byteOrder = getDataByteOrder();
-		String outputFormat = getOutputFormat();
-		if (outputFormat == null) {
-			outputFormat = "";
-		}
 
-		String pattern = "SOURCENIC{0}/BG/BYTEORDER={1}/FC={2}/MGRP={3}/PORT={4,number,#}/VLAN={5,number,#} OUT={6}";
-		String cmdLine = MessageFormat.format(pattern, switches, byteOrder, outputFormat, getMcastAddress(), getPort(), getVlan(), outputName);
+		String pattern = "SOURCENIC{0}/BG OUT={1}";
+		String cmdLine = MessageFormat.format(pattern, switches, outputName);
 
 		return cmdLine;
 	}
@@ -85,21 +94,57 @@ public class SddsNxmBlock extends AbstractNxmBlock<sourcenic> {
 		return SddsPreferences.VLAN.getValue(getPreferences());
 	}
 
+	public void setVlan(int vlan) {
+		SddsPreferences.VLAN.setValue(getPreferences(), vlan);
+	}
+
 	public int getPort() {
 		return SddsPreferences.PORT.getValue(getPreferences());
 	}
 
+	public void setPort(int port) {
+		SddsPreferences.PORT.setValue(getPreferences(), port);
+	}
+	
 	public String getMcastAddress() {
-		if (SddsPreferences.MCAST_ADDRESS.isDefault(getPreferences())) {
-			return null;
-		}
 		return SddsPreferences.MCAST_ADDRESS.getValue(getPreferences());
 	}
+	
+	public void setMcastAddress(String mcastAddress) {
+		if (mcastAddress != null) {
+			SddsPreferences.MCAST_ADDRESS.setValue(getPreferences(), mcastAddress);
+		} else {
+			SddsPreferences.MCAST_ADDRESS.setToDefault(getPreferences());
+		}
+	}
 
+	public String getInterfaceName() {
+		return SddsPreferences.INTERFACE_NAME.getValue(getPreferences());
+	}
+
+	public void setInterfaceName(String interfaceName) {
+		if (interfaceName != null) {
+			SddsPreferences.INTERFACE_NAME.setValue(getPreferences(), interfaceName);
+		} else {
+			SddsPreferences.INTERFACE_NAME.setToDefault(getPreferences());
+		}
+	}
+	
 	public String getOutputFormat() {
 		return SddsPreferences.OUTPUT_FORMAT.getValue(getPreferences());
 	}
 
+	public void setOutputFormat(String outputFormat) {
+		if (outputFormat != null) {
+			SddsPreferences.OUTPUT_FORMAT.setValue(getPreferences(), outputFormat);
+		} else {
+			SddsPreferences.OUTPUT_FORMAT.setToDefault(getPreferences());
+		}
+	}
+
+	/**
+	 * @return current data byte order to use
+	 */
 	@Nullable
 	public ByteOrder getDataByteOrder() {
 		String newValue = SddsPreferences.BYTE_ORDER.getValue(getPreferences());
@@ -114,26 +159,42 @@ public class SddsNxmBlock extends AbstractNxmBlock<sourcenic> {
 		}
 	}
 
-	public int getPipeSize() {
-		return SddsPreferences.PIPE_SIZE.getValue(getPreferences());
-	}
-
-	@NonNull
-	public SddsNxmBlockSettings getSettings() {
-		return new SddsNxmBlockSettings(getPreferences());
-	}
-
-	public void applySettings(SddsNxmBlockSettings newSettings) {
-		ByteOrder byteOrder = newSettings.getDataByteOrder();
-		setDataByteOrder(byteOrder);
-	}
-
 	public void setDataByteOrder(ByteOrder byteOrder) {
 		if (byteOrder != null) {
 			SddsPreferences.BYTE_ORDER.setValue(getPreferences(), byteOrder.toString());
 		} else {
 			SddsPreferences.BYTE_ORDER.setToDefault(getPreferences());
 		}
+	}
+	
+	public int getPipeSize() {
+		return SddsPreferences.PIPE_SIZE.getValue(getPreferences());
+	}
+
+	public void setPipeSize(int pipeSize) {
+		SddsPreferences.PIPE_SIZE.setValue(getPreferences(), pipeSize);
+	}
+	
+	@NonNull
+	public SddsNxmBlockSettings getSettings() {
+		return new SddsNxmBlockSettings(getPreferences());
+	}
+
+	public void applySettings(@NonNull SddsNxmBlockSettings newSettings) {
+		ByteOrder byteOrder = newSettings.getDataByteOrder();
+		setDataByteOrder(byteOrder);
+		String interfaceName = newSettings.getInterfaceName();
+		setInterfaceName(interfaceName);
+		String mcastAddress = newSettings.getMcastAddress();
+		setMcastAddress(mcastAddress);
+		int port = newSettings.getPort();
+		setPort(port);
+		int vlan = newSettings.getVlan();
+		setVlan(vlan);
+		String outputFormat = newSettings.getOutputFormat();
+		setOutputFormat(outputFormat);
+		int pipeSize = newSettings.getPipeSize();
+		setPipeSize(pipeSize);
 	}
 
 	@Override
