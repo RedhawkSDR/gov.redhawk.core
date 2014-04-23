6f0cc44d2795b0342c44692ba11a8a4f505e1f08
Mark Leone <mal@axiosengineering.com>
2014-04-23 13:14:54 -0400

    Fix NPE when deleting simple property from struct property in struct sequence property. refs 2041. More verification needed, as I'm unable to re-produce the problem now when I revert to code without this fix, though I did see the problem befoer making the fix.

--- plugins/gov.redhawk.prf.ui/src/gov/redhawk/prf/internal/ui/editor/PrfActionBarContributor.java
+++ plugins/gov.redhawk.prf.ui/src/gov/redhawk/prf/internal/ui/editor/PrfActionBarContributor.java
@@ -345,7 +345,7 @@ public class PrfActionBarContributor extends EditingDomainActionBarContributor i
 
 	@Override
 	public void init(final IActionBars actionBars) {
-//		final IAction d = actionBars.getGlobalActionHandler(ActionFactory.DELETE.getId());
+		final IAction d = actionBars.getGlobalActionHandler(ActionFactory.DELETE.getId());
 		final IAction cu = actionBars.getGlobalActionHandler(ActionFactory.CUT.getId());
 		final IAction cp = actionBars.getGlobalActionHandler(ActionFactory.COPY.getId());
 		final IAction pt = actionBars.getGlobalActionHandler(ActionFactory.PASTE.getId());
@@ -353,6 +353,7 @@ public class PrfActionBarContributor extends EditingDomainActionBarContributor i
 		actionBars.setGlobalActionHandler(ActionFactory.CUT.getId(), cu);
 		actionBars.setGlobalActionHandler(ActionFactory.COPY.getId(), cp);
 		actionBars.setGlobalActionHandler(ActionFactory.PASTE.getId(), pt);
+		actionBars.setGlobalActionHandler(ActionFactory.DELETE.getId(), d);
 	}
 
 	/**
