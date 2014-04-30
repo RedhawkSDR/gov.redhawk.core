0f2430751329119e8a34a931c3997e85193d2d00
Erik Englund <erik.englund@axiosengineering.com>
2014-04-30 18:23:08 -0400

    refs #2050 Add validation to refresh text field.

--- plugins/gov.redhawk.ui.views.monitor/src/gov/redhawk/ui/views/internal/monitor/ports/PortMonitorViewConfigDialog.java
+++ plugins/gov.redhawk.ui.views.monitor/src/gov/redhawk/ui/views/internal/monitor/ports/PortMonitorViewConfigDialog.java
@@ -11,6 +11,7 @@
  */
 package gov.redhawk.ui.views.internal.monitor.ports;
 
+import gov.redhawk.sca.util.PropertyChangeSupport;
 import gov.redhawk.ui.views.monitor.model.ports.Monitor;
 import gov.redhawk.ui.views.monitor.model.ports.MonitorRegistry;
 import gov.redhawk.ui.views.monitor.model.ports.PortConnectionMonitor;
@@ -21,23 +22,31 @@ import gov.redhawk.ui.views.monitor.ports.Column;
 import gov.redhawk.ui.views.monitor.ports.PortMonitorView;
 import gov.redhawk.ui.views.monitor.ports.StatisticsColumns;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-import org.eclipse.jface.viewers.ArrayContentProvider;
-import org.eclipse.jface.viewers.CheckStateChangedEvent;
+import org.eclipse.core.databinding.Binding;
+import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.beans.BeansObservables;
+import org.eclipse.core.databinding.beans.PojoProperties;
+import org.eclipse.core.databinding.observable.value.IValueChangeListener;
+import org.eclipse.core.databinding.observable.value.ValueChangeEvent;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.jface.databinding.fieldassist.ControlDecorationSupport;
+import org.eclipse.jface.databinding.swt.WidgetProperties;
+import org.eclipse.jface.databinding.viewers.ViewerProperties;
+import org.eclipse.jface.databinding.viewers.ViewerSupport;
+import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.viewers.CheckboxTableViewer;
-import org.eclipse.jface.viewers.ICheckStateListener;
-import org.eclipse.jface.viewers.ICheckStateProvider;
-import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerSorter;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.VerifyEvent;
-import org.eclipse.swt.events.VerifyListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Group;
@@ -55,27 +64,78 @@ import CF.DataType;
  */
 public class PortMonitorViewConfigDialog extends ViewSettingsDialog {
 
-	private final PortMonitorView view;
+	private static class State {
+		private long refresh = 10;
+		private Set<Column> checked = new HashSet<Column>();
+		private List<Column> input = new ArrayList<Column>();
+		private PropertyChangeSupport pcs = new PropertyChangeSupport(this);
 
-	private CheckboxTableViewer viewer;
+		public void addPropertyChangeListener(java.beans.PropertyChangeListener listener) {
+			pcs.addPropertyChangeListener(listener);
+		}
+
+		public void removePropertyChangeListener(java.beans.PropertyChangeListener listener) {
+			pcs.removePropertyChangeListener(listener);
+		}
+
+		public long getRefresh() {
+			return refresh;
+		}
+
+		public void setRefresh(long refresh) {
+			long oldValue = this.refresh;
+			this.refresh = refresh;
+			pcs.firePropertyChange("refresh", oldValue, refresh);
+		}
+
+		public Set<Column> getChecked() {
+			return checked;
+		}
+
+		public void setChecked(Set<Column> checked) {
+			Set<Column> oldValue = this.checked;
+			this.checked = checked;
+			pcs.firePropertyChange("checked", oldValue, checked);
+		}
+
+		public List<Column> getInput() {
+			return input;
+		}
+
+		public void setInput(List<Column> input) {
+			List<Column> oldValue = input;
+			this.input = input;
+			pcs.firePropertyChange("input", oldValue, input);
+		}
+
+	}
+
+	private final State state = new State();
+
+	private final PortMonitorView view;
 
 	private final MonitorRegistry data;
 
-	private Text refreshText;
+	private DataBindingContext context = new DataBindingContext();
 
-	private final Map<Column, Boolean> columnMap = new HashMap<Column, Boolean>();
+	private Text refreshText;
 
 	public PortMonitorViewConfigDialog(final Shell parentShell, final PortMonitorView view, final MonitorRegistry data) {
 		super(parentShell);
 		this.view = view;
 		this.data = data;
+		state.refresh = view.getRefreshDelta() / 1000;
 		initializeColumnMap();
 	}
 
 	private void initializeColumnMap() {
-		this.columnMap.clear();
+		state.checked.clear();
+		state.input.clear();
 		for (final Column c : StatisticsColumns.DEFAULT_COLUMNS) {
-			this.columnMap.put(c, this.view.hasColumn(c));
+			if (view.hasColumn(c)) {
+				state.checked.add(c);
+			}
+			state.input.add(c);
 		}
 		for (final Monitor m : this.data.getMonitors()) {
 			if (m instanceof PortSupplierMonitor) {
@@ -112,16 +172,19 @@ public class PortMonitorViewConfigDialog extends ViewSettingsDialog {
 	}
 
 	private void addDataType(final DataType type) {
-		if (!this.columnMap.containsKey(type.id)) {
+		if (!state.input.contains(type.id)) {
 			final DataTypeColumn dataTypeColumn = new DataTypeColumn(type);
-			this.columnMap.put(dataTypeColumn, this.view.hasColumn(dataTypeColumn));
+			if (this.view.hasColumn(dataTypeColumn)) {
+				state.checked.add(dataTypeColumn);
+			}
+			state.input.add(dataTypeColumn);
 		}
 	}
 
 	@Override
 	protected Control createDialogArea(final Composite parent) {
 		final Composite composite = (Composite) super.createDialogArea(parent);
-		//add controls to composite as necessary
+		// add controls to composite as necessary
 		createRefreshGroup(composite);
 		createColumnGroup(composite);
 
@@ -138,16 +201,21 @@ public class PortMonitorViewConfigDialog extends ViewSettingsDialog {
 		final Label label = new Label(group, SWT.None);
 		label.setText("Refresh Interval (sec):");
 
-		this.refreshText = new Text(group, SWT.BORDER);
-		this.refreshText.setText(String.valueOf(this.view.getRefreshDelta() / 1000));
-		this.refreshText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		this.refreshText.addVerifyListener(new VerifyListener() {
-
+		refreshText = new Text(group, SWT.BORDER);
+		refreshText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		Binding binding = context.bindValue(WidgetProperties.text(SWT.Modify).observe(refreshText), BeansObservables.observeValue(state, "refresh"));
+		binding.getValidationStatus().addValueChangeListener(new IValueChangeListener() {
+			
 			@Override
-			public void verifyText(final VerifyEvent e) {
-				e.doit = Character.isDigit(e.character);
+			public void handleValueChange(ValueChangeEvent event) {
+				IStatus status = (IStatus) event.getObservableValue().getValue();
+				Button button = getButton(IDialogConstants.OK_ID);
+				if (button != null) {
+					button.setEnabled(status.isOK());
+				}
 			}
 		});
+		ControlDecorationSupport.create(binding, SWT.TOP | SWT.LEFT);
 	}
 
 	private void createColumnGroup(final Composite parent) {
@@ -155,15 +223,13 @@ public class PortMonitorViewConfigDialog extends ViewSettingsDialog {
 		group.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		group.setText("Column Configuration");
 		group.setLayout(new GridLayout(1, false));
-		this.viewer = CheckboxTableViewer.newCheckList(group, SWT.BORDER | SWT.FULL_SELECTION | SWT.V_SCROLL);
-		final Table table = this.viewer.getTable();
+		CheckboxTableViewer viewer = CheckboxTableViewer.newCheckList(group, SWT.BORDER | SWT.FULL_SELECTION | SWT.V_SCROLL);
+		final Table table = viewer.getTable();
 		final GridData gd_table = new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1);
 		gd_table.minimumHeight = 200;
 		table.setLayoutData(gd_table);
-		this.viewer.getTable().setHeaderVisible(false);
-		this.viewer.setContentProvider(new ArrayContentProvider());
-		this.viewer.setLabelProvider(new LabelProvider());
-		this.viewer.setSorter(new ViewerSorter() {
+		viewer.getTable().setHeaderVisible(false);
+		viewer.setSorter(new ViewerSorter() {
 			@Override
 			public int compare(final Viewer viewer, final Object e1, final Object e2) {
 				final Column c1 = (Column) e1;
@@ -171,37 +237,19 @@ public class PortMonitorViewConfigDialog extends ViewSettingsDialog {
 				return c1.getName().compareToIgnoreCase(c2.getName());
 			}
 		});
-		this.viewer.setCheckStateProvider(new ICheckStateProvider() {
-
-			@Override
-			public boolean isGrayed(final Object element) {
-				return false;
-			}
-
-			@Override
-			public boolean isChecked(final Object element) {
-				final Column c = (Column) element;
-				return PortMonitorViewConfigDialog.this.columnMap.get(c);
-			}
-		});
-		this.viewer.addCheckStateListener(new ICheckStateListener() {
-
-			@Override
-			public void checkStateChanged(final CheckStateChangedEvent event) {
-				PortMonitorViewConfigDialog.this.columnMap.put(((Column) event.getElement()), event.getChecked());
-			}
-		});
-		this.viewer.setInput(this.columnMap.keySet());
+		ViewerSupport.bind(viewer, BeansObservables.observeList(state, "input"), PojoProperties.value("name"));
+		context.bindSet(ViewerProperties.checkedElements(Column.class).observe(viewer), BeansObservables.observeSet(state, "checked"));
 	}
 
 	@Override
 	protected void okPressed() {
-		this.view.setRefreshDelta(Long.valueOf(this.refreshText.getText()) * 1000L);
-		for (final Entry<Column, Boolean> entry : this.columnMap.entrySet()) {
-			if (entry.getValue()) {
-				this.view.addColumn(entry.getKey());
+		this.view.setRefreshDelta(state.refresh * 1000L);
+
+		for (Column c : state.input) {
+			if (state.checked.contains(c)) {
+				view.addColumn(c);
 			} else {
-				this.view.removeColumn(entry.getKey());
+				view.removeColumn(c);
 			}
 		}
 		super.okPressed();
@@ -210,11 +258,9 @@ public class PortMonitorViewConfigDialog extends ViewSettingsDialog {
 	@Override
 	protected void performDefaults() {
 		super.performDefaults();
-		this.refreshText.setText("10");
+		refreshText.setText("10");
 		initializeColumnMap();
-		for (final Column c : StatisticsColumns.DEFAULT_COLUMNS) {
-			this.columnMap.put(c, true);
-		}
-		this.viewer.refresh();
+		HashSet<Column> newSet = new HashSet<Column>(Arrays.asList(StatisticsColumns.DEFAULT_COLUMNS));
+		state.setChecked(newSet);
 	}
 }
