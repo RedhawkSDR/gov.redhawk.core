cc66b8caccfddb089e1db43482be9d7eb3bdb3d9
Devin Nelson <devin.nelson@axiosengineering.com>
2014-05-21 16:48:31 -0400

    edits to Connection Manager junit test

--- tests/gov.redhawk.bulkio.util.tests/src/gov/redhawk/bulkio/util/tests/internal/ConnectionManagerTest.java
+++ tests/gov.redhawk.bulkio.util.tests/src/gov/redhawk/bulkio/util/tests/internal/ConnectionManagerTest.java
@@ -24,12 +24,12 @@ import CF.PortPackage.InvalidPort;
 import CF.PortPackage.OccupiedPort;
 
 public class ConnectionManagerTest {
-	
+
 	private static OrbSession session;
-	
+
 	private static class MyPort implements PortOperations {
-		
-		public int connections =0;
+
+		public int connections = 0;
 
 		@Override
 		public void connectPort(Object connection, String connectionId) throws InvalidPort, OccupiedPort {
@@ -40,9 +40,9 @@ public class ConnectionManagerTest {
 		public void disconnectPort(String connectionId) throws InvalidPort {
 			connections--;
 		}
-		
+
 	}
-	
+
 	private static class TestDataDoublePort extends AbstractBulkIOPort implements dataDoubleOperations {
 		@Override
 		public void pushPacket(double[] data, PrecisionUTCTime T, boolean EOS, String streamID) {
@@ -56,27 +56,24 @@ public class ConnectionManagerTest {
 
 	private MyPort port;
 	private Object portRef;
-	
+
 	@Before
 	public void initPort() throws Exception {
 		POA poa = session.getPOA();
-		port = new MyPort(); 
-		portRef =poa.servant_to_reference(new PortPOATie(port));
+		port = new MyPort();
+		portRef = poa.servant_to_reference(new PortPOATie(port));
 	}
-	
+
 	@After
 	public void disposePort() throws Exception {
 		portRef._release();
 		portRef = null;
 		port = null;
 	}
-	
-	
+
 	@AfterClass
 	public static void dispose() {
-		if (session != null) {
-			session.dispose();
-		}
+		session.dispose();
 	}
 
 	@Test
@@ -84,26 +81,26 @@ public class ConnectionManagerTest {
 		IBulkIOPortConnectionManager connectionManager = BulkIOUtilActivator.getBulkIOPortConnectionManager();
 		TestDataDoublePort receivePort = new TestDataDoublePort();
 		connectionManager.connect(portRef.toString(), BulkIOType.DOUBLE, receivePort);
-		Assert.assertEquals("Only one connection expected", 1, port.connections); 
-		
+		Assert.assertEquals("Only one connection expected", 1, port.connections);
+
 		TestDataDoublePort receivePort2 = new TestDataDoublePort();
 		connectionManager.connect(portRef.toString(), BulkIOType.DOUBLE, receivePort2);
-		Assert.assertEquals("Only one connection expected", 1, port.connections); 
-		
+		Assert.assertEquals("Only one connection expected", 1, port.connections);
+
 		TestDataDoublePort receivePort3 = new TestDataDoublePort();
 		connectionManager.connect(portRef.toString(), BulkIOType.DOUBLE, receivePort3);
-		Assert.assertEquals("Only one connection expected", 1, port.connections); 
-		
+		Assert.assertEquals("Only one connection expected", 1, port.connections);
+
 		connectionManager.disconnect(portRef.toString(), BulkIOType.DOUBLE, receivePort);
 		Assert.assertEquals("Only one connection expected", 1, port.connections);
-		
+
 		connectionManager.disconnect(portRef.toString(), BulkIOType.DOUBLE, receivePort2);
 		Assert.assertEquals("Only one connection expected", 1, port.connections);
-		
+
 		connectionManager.disconnect(portRef.toString(), BulkIOType.DOUBLE, receivePort3);
-		Assert.assertEquals("No connections expected", 0, port.connections); 
+		Assert.assertEquals("No connections expected", 0, port.connections);
 	}
-	
+
 	@Test
 	public void testConnectionManagerConnectDisconnectWithConnectionID() throws CoreException {
 		IBulkIOPortConnectionManager connectionManager = BulkIOUtilActivator.getBulkIOPortConnectionManager();
@@ -111,43 +108,40 @@ public class ConnectionManagerTest {
 		String connectionID = "connection";
 		String connection1 = connectionManager.connect(portRef.toString(), BulkIOType.DOUBLE, receivePort, connectionID);
 		Assert.assertEquals(connectionID, connection1);
-		Assert.assertEquals("Only one connection expected", 1, port.connections); 
-		
+		Assert.assertEquals("Only one connection expected", 1, port.connections);
+
 		TestDataDoublePort receivePort2 = new TestDataDoublePort();
 		String connection2 = connectionManager.connect(portRef.toString(), BulkIOType.DOUBLE, receivePort2, connectionID);
 		Assert.assertEquals(connectionID, connection2);
-		Assert.assertEquals("Only one connection expected", 1, port.connections); 
-		
+		Assert.assertEquals("Only one connection expected", 1, port.connections);
+
 		TestDataDoublePort receivePort3 = new TestDataDoublePort();
 		String connection3 = connectionManager.connect(portRef.toString(), BulkIOType.DOUBLE, receivePort3, connectionID);
 		Assert.assertEquals(connectionID, connection3);
-		Assert.assertEquals("Only one connection expected", 1, port.connections); 
-		
+		Assert.assertEquals("Only one connection expected", 1, port.connections);
+
 		connectionManager.disconnect(portRef.toString(), BulkIOType.DOUBLE, receivePort, connectionID);
 		Assert.assertEquals("Only one connection expected", 1, port.connections);
-		
+
 		connectionManager.disconnect(portRef.toString(), BulkIOType.DOUBLE, receivePort2, connectionID);
 		Assert.assertEquals("Only one connection expected", 1, port.connections);
-		
+
 		connectionManager.disconnect(portRef.toString(), BulkIOType.DOUBLE, receivePort3, connectionID);
-		Assert.assertEquals("No connections expected", 0, port.connections); 
+		Assert.assertEquals("No connections expected", 0, port.connections);
 	}
-	
+
 	@Test
 	public void testGetExternalPort() throws CoreException {
 		IBulkIOPortConnectionManager connectionManager = BulkIOUtilActivator.getBulkIOPortConnectionManager();
-		
-		
+
 		Assert.assertNull(connectionManager.getExternalPort(null, null));
 		Assert.assertNull(connectionManager.getExternalPort(null, BulkIOType.DOUBLE));
 		Assert.assertNull(connectionManager.getExternalPort(portRef.toString(), BulkIOType.DOUBLE));
-		
+
 		TestDataDoublePort receivePort = new TestDataDoublePort();
 		connectionManager.connect(portRef.toString(), BulkIOType.DOUBLE, receivePort);
 		Assert.assertEquals("Only one connection expected", 1, port.connections);
 		Assert.assertNotNull(connectionManager.getExternalPort(portRef.toString(), BulkIOType.DOUBLE));
 	}
-	
-
 
 }
