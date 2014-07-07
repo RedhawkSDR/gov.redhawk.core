8ea14a9ddcb1675d319ad024431d268d5226efb9
Erik Englund <erik.englund@axiosengineering.com>
2014-07-07 16:52:11 -0400

    IDE-794 Added TX to list of supported Tuner types. #time 5m

--- plugins/gov.redhawk.frontend.ui/src/gov/redhawk/frontend/ui/FrontEndUIActivator.java
+++ plugins/gov.redhawk.frontend.ui/src/gov/redhawk/frontend/ui/FrontEndUIActivator.java
@@ -43,9 +43,13 @@ public class FrontEndUIActivator extends AbstractUIPlugin {
 	public FrontEndUIActivator() {
 	}
 
-	public static final List<String> SUPPORTED_TUNER_TYPES = Collections.unmodifiableList(new ArrayList<String>(Arrays.asList(new String[] {
-		FRONTEND.TUNER_TYPE_RX_DIGITIZER.value, FRONTEND.TUNER_TYPE_CHANNELIZER.value, FRONTEND.TUNER_TYPE_DDC.value, FRONTEND.TUNER_TYPE_RX.value,
-		FRONTEND.TUNER_TYPE_RX_DIGITIZER_CHANNELIZER.value })));
+	public static final List<String> SUPPORTED_TUNER_TYPES = getSupportedTuner();
+	
+	private static List<String> getSupportedTuner() {
+		return Collections.unmodifiableList(new ArrayList<String>(Arrays.asList(new String[] {
+			FRONTEND.TUNER_TYPE_RX_DIGITIZER.value, FRONTEND.TUNER_TYPE_CHANNELIZER.value, FRONTEND.TUNER_TYPE_DDC.value, FRONTEND.TUNER_TYPE_RX.value,
+			FRONTEND.TUNER_TYPE_RX_DIGITIZER_CHANNELIZER.value, FRONTEND.TUNER_TYPE_TX.value })));
+	}
 
 	public static enum ALLOCATION_MODE {
 		TUNER,
