da04608392a4eaf45e31e824255ba836a4631a56
Devin Nelson <devin.nelson@axiosengineering.com>
2015-04-21 15:18:41 -0400

    removed emf generated tests

--- tests/gov.redhawk.sca.model.tests/src/gov/redhawk/model/sca/tests/ScaTests.java
+++ tests/gov.redhawk.sca.model.tests/src/gov/redhawk/model/sca/tests/ScaTests.java
@@ -61,7 +61,6 @@ public class ScaTests extends TestSuite {
 		suite.addTestSuite(ScaWaveformTest.class);
 		suite.addTestSuite(ScaWaveformFactoryTest.class);
 		suite.addTestSuite(ScaStructSequencePropertyTest.class);
-		suite.addTestSuite(ScaEventChannelTest.class);
 		return suite;
 	}
 
