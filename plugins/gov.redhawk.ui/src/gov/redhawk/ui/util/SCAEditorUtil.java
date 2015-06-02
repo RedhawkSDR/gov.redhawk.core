RedhawkSDR/gov.redhawk.core

commit eb0b84d566633d7bb60158ad77cf4ca8e393f78c

IDE-857 Adding support for editing files outside the workspace.

plugins/gov.redhawk.ui/src/gov/redhawk/ui/util/SCAEditorUtil.java
@@ -39,7 +39,7 @@ public class SCAEditorUtil {
 	 */
 	public static boolean isEditableResource(ScaFormPage page, Resource resource) {
 		boolean retVal = false;
-		if (resource != null && resource.getURI().isPlatform()) {
+		if (resource != null) {
 			retVal = !page.getEditingDomain().isReadOnly(resource);
 		}
 		return retVal;

plugins/gov.redhawk.ui/src/gov/redhawk/internal/ui/editor/validation/ValidatingEContentAdapter.java
@@ -29,6 +29,7 @@ import gov.redhawk.ui.editor.FormOutlinePage;
 import gov.redhawk.ui.editor.SCAFormEditor;
 import gov.redhawk.ui.validation.ValidatingService;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -214,7 +215,8 @@ public class ValidatingEContentAdapter extends EContentAdapter {
 	 * @param messageManager
 	 */
 	private void validateResourceSet(final ResourceSet resourceSet, final IMessageManager messageManager, final DiagnosticChain diagnosticChain) {
-		for (final Object o : resourceSet.getResources().toArray()) {
+		List<Resource> resources = new ArrayList<Resource>(resourceSet.getResources());
+		for (final Object o : resources) {
 			final Resource resource = (Resource) o;
 			validateResource(resource, messageManager, diagnosticChain);
 		}

commit 1525039f5a2700f5129f06da229d7629764bc353

IDE-1285 Editors opened from items in the Target SDR should be read-only
Fixes a bug introduced in eb0b84d for IDE-857.

plugins/gov.redhawk.ui/src/gov/redhawk/ui/util/SCAEditorUtil.java
@@ -12,6 +12,7 @@
 package gov.redhawk.ui.util;
 
 import gov.redhawk.ui.editor.ScaFormPage;
+import mil.jpeojtrs.sca.util.ScaFileSystemConstants;
 
 import org.eclipse.emf.ecore.resource.Resource;
 
@@ -39,7 +40,7 @@ public class SCAEditorUtil {
 	 */
 	public static boolean isEditableResource(ScaFormPage page, Resource resource) {
 		boolean retVal = false;
-		if (resource != null) {
+		if (resource != null && !ScaFileSystemConstants.SCHEME.equals(resource.getURI().scheme())) {
 			retVal = !page.getEditingDomain().isReadOnly(resource);
 		}
 		return retVal;