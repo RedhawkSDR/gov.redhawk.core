233ac6268d6d3537bc162aed55f9d69e9b24af45
Daniel Wille <daniel.wille@axiosengineering.com>
2015-06-29 14:26:21 -0400

    IDE-1022 Tests for OS version strings

---
+++ tests/gov.redhawk.swt.browser.tests/src/gov/redhawk/swt/browser/BrowserInitializerTest.java
@@ -0,0 +1,33 @@
+package gov.redhawk.swt.browser;
+
+import gov.redhawk.swt.browser.OsVersionParser;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.osgi.framework.Version;
+
+public class BrowserInitializerTest {
+
+	/**
+	 * Tests a few versions strings for RHEL
+	 */
+	@Test
+	public void testRedHatVersions() {
+		Assert.assertEquals(new Version(5, 11, 0), OsVersionParser.getRedhatVersion("Red Hat Enterprise Linux Server release 5.11 (Tikanga)"));
+		Assert.assertEquals(new Version(6, 4, 0), OsVersionParser.getRedhatVersion("Red Hat Enterprise Linux Server release 6.4 (Santiago)"));
+	}
+
+	/**
+	 * Tests a few version strings for CentOS
+	 */
+	@Test
+	public void testCentOSVersions() {
+		Assert.assertEquals(new Version(6, 0, 0), OsVersionParser.getRedhatVersion("CentOS Linux release 6.0 (Final)"));
+		Assert.assertEquals(new Version(6, 4, 0), OsVersionParser.getRedhatVersion("CentOS release 6.4 (Final)"));
+		Assert.assertEquals(new Version(6, 5, 0), OsVersionParser.getRedhatVersion("CentOS release 6.5 (Final)"));
+		Assert.assertEquals(new Version(6, 6, 0), OsVersionParser.getRedhatVersion("CentOS release 6.6 (Final)"));
+		Assert.assertEquals(new Version(7, 0, 0), OsVersionParser.getRedhatVersion("CentOS Linux release 7.0.1406 (Core)"));
+		Assert.assertEquals(new Version(7, 1, 0), OsVersionParser.getRedhatVersion("CentOS Linux release 7.1.1503 (Core) "));
+	}
+
+}
