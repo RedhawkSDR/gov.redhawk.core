f9c71406a732d1e41d9e59122967b9bcc7a72dbb
Daniel Wille <daniel.wille@axiosengineering.com>
2016-03-31 18:08:29 -0400

    IDE-1566 Fix refresh context menu for event channel and waveform folders
    The logic for refreshing the waveform and event channel containers was swapped between the two classes.

--- plugins/gov.redhawk.sca.ui/src/gov/redhawk/sca/internal/ui/ScaEventChannelsContainerItemProviderAdapterFactory.java
+++ plugins/gov.redhawk.sca.ui/src/gov/redhawk/sca/internal/ui/ScaEventChannelsContainerItemProviderAdapterFactory.java
@@ -1,30 +1,24 @@
-/** 
- * This file is protected by Copyright. 
+/**
+ * This file is protected by Copyright.
  * Please refer to the COPYRIGHT file distributed with this source distribution.
- * 
+ *
  * This file is part of REDHAWK IDE.
- * 
- * All rights reserved.  This program and the accompanying materials are made available under 
+ *
+ * All rights reserved.  This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html.
- *
  */
 package gov.redhawk.sca.internal.ui;
 
 import gov.redhawk.model.sca.IRefreshable;
 import gov.redhawk.model.sca.RefreshDepth;
 import gov.redhawk.model.sca.ScaDomainManager;
-import gov.redhawk.model.sca.ScaWaveform;
-import gov.redhawk.model.sca.commands.ScaModelCommandWithResult;
-import gov.redhawk.model.sca.provider.ScaWaveformsContainerItemProvider;
+import gov.redhawk.model.sca.provider.ScaEventChannelsContainerItemProvider;
 
 import org.eclipse.core.runtime.IAdapterFactory;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.SubMonitor;
 
-/**
- * 
- */
 public class ScaEventChannelsContainerItemProviderAdapterFactory implements IAdapterFactory {
 
 	private static final Class< ? >[] LIST = new Class< ? >[] {
@@ -41,12 +35,10 @@ public class ScaEventChannelsContainerItemProviderAdapterFactory implements IAda
 
 		@Override
 		public void refresh(final IProgressMonitor monitor, final RefreshDepth depth) throws InterruptedException {
-			final SubMonitor subMonitor = SubMonitor.convert(monitor, "Fetching waveforms", 100);
+			final SubMonitor subMonitor = SubMonitor.convert(monitor, "Fetching event channels", 100);
 
 			switch (depth) {
 			case FULL:
-				refreshFull(subMonitor.newChild(100));
-				break;
 			case CHILDREN:
 			case SELF:
 				refreshStandard(subMonitor.newChild(100));
@@ -57,47 +49,21 @@ public class ScaEventChannelsContainerItemProviderAdapterFactory implements IAda
 		}
 
 		private void refreshStandard(final IProgressMonitor monitor) throws InterruptedException {
-			this.domain.fetchWaveforms(monitor);
-		}
-
-		private void refreshFull(final IProgressMonitor monitor) throws InterruptedException {
-			final SubMonitor subMonitor = SubMonitor.convert(monitor, "Fetching waveforms fully", 2);
-			refreshStandard(subMonitor.newChild(1));
-			final ScaWaveform[] waveforms = ScaModelCommandWithResult.execute(this.domain, new ScaModelCommandWithResult<ScaWaveform[]>() {
-
-				@Override
-				public void execute() {
-					setResult(Refresher.this.domain.getWaveforms().toArray(new ScaWaveform[Refresher.this.domain.getWaveforms().size()]));
-				}
-			});
-			final SubMonitor refreshMonitor = subMonitor.newChild(1);
-			if (waveforms != null) {
-				refreshMonitor.beginTask("Refreshing waveforms", waveforms.length);
-				for (final ScaWaveform waveform : waveforms) {
-					waveform.refresh(refreshMonitor.newChild(1), RefreshDepth.SELF);
-				}
-			}
-
+			this.domain.fetchEventChannels(monitor);
 		}
 
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
 	@Override
 	public Object getAdapter(final Object adaptableObject, @SuppressWarnings("rawtypes") final Class adapterType) {
-		if (adaptableObject instanceof ScaWaveformsContainerItemProvider) {
+		if (adaptableObject instanceof ScaEventChannelsContainerItemProvider) {
 			if (adapterType == IRefreshable.class) {
-				return new Refresher((ScaDomainManager) ((ScaWaveformsContainerItemProvider) adaptableObject).getTarget());
+				return new Refresher((ScaDomainManager) ((ScaEventChannelsContainerItemProvider) adaptableObject).getTarget());
 			}
 		}
 		return null;
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
 	@Override
 	public Class< ? >[] getAdapterList() {
 		return ScaEventChannelsContainerItemProviderAdapterFactory.LIST;
