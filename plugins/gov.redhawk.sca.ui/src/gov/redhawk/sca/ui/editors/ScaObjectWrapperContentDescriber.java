33edaa55f12e4126f03b55dcbd5c96acef52df9c
Daniel Wille <daniel.wille@axiosengineering.com>
2016-05-13 15:20:17 -0400

    IDE-1593 Waveforms sometimes opened with wrong editor
    The content describer modified in this commit wouldn't accurately identify
    objects if their profiles hadn't loaded. This only manifested in the 2.0-series
    where a recent code change caused the content describer to sometimes evaluate
    an object before profile loading had occurred.

--- plugins/gov.redhawk.sca.ui/src/gov/redhawk/sca/ui/editors/ScaObjectWrapperContentDescriber.java
+++ plugins/gov.redhawk.sca.ui/src/gov/redhawk/sca/ui/editors/ScaObjectWrapperContentDescriber.java
@@ -20,6 +20,7 @@ import gov.redhawk.sca.ui.ScaUiPlugin;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.PatternSyntaxException;
 
 import mil.jpeojtrs.sca.util.QueryParser;
 import mil.jpeojtrs.sca.util.ScaFileSystemConstants;
@@ -32,44 +33,41 @@ import org.eclipse.core.runtime.IExecutableExtension;
 import org.eclipse.emf.common.CommonPlugin;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.ui.IEditorInput;
 
 /**
  * @since 2.2
  */
 public class ScaObjectWrapperContentDescriber implements IScaContentDescriber, IExecutableExtension {
+
 	public static final String PARAM_PROFILE_FILENAME = "profileFilename";
+
 	private Map<String, String> params;
 
 	public int describe(final Object contents) throws IOException {
 		if (this.params == null) {
-			return IScaContentDescriber.INDETERMINATE;
+			return IScaContentDescriber.INVALID;
+		}
+		final String profileFileName = this.params.get(ScaObjectWrapperContentDescriber.PARAM_PROFILE_FILENAME);
+		if (profileFileName == null) {
+			return IScaContentDescriber.INVALID;
 		}
 
 		if (!(contents instanceof ProfileObjectWrapper< ? >)) {
 			return IScaContentDescriber.INVALID;
 		}
-
 		final ProfileObjectWrapper< ? > obj = (ProfileObjectWrapper< ? >) contents;
 
-		final String profileFileName = this.params.get(ScaObjectWrapperContentDescriber.PARAM_PROFILE_FILENAME);
 		try {
-			if (profileFileName != null) {
-				final Object scaObj = obj.getProfileObj();
-				if (scaObj instanceof EObject) {
-					final EObject eObj = (EObject) scaObj;
-					final Resource resource = eObj.eResource();
-					if (resource != null && resource.getURI() != null && resource.getURI().lastSegment() != null
-					        && resource.getURI().lastSegment().matches(profileFileName)) {
-						return IScaContentDescriber.VALID;
-					}
-				}
+			URI profileUri = obj.getProfileURI();
+			if (profileUri == null || profileUri.lastSegment() == null || !profileUri.lastSegment().matches(profileFileName)) {
+				return IScaContentDescriber.INVALID;
 			}
-		} catch (final Exception e) { // SUPPRESS CHECKSTYLE Logged
-			ScaUiPlugin.logError("Unable to describe content", e);
+		} catch (final PatternSyntaxException e) {
+			ScaUiPlugin.logError("Invalid profile filename regex in extension point", e);
 		}
-		return IScaContentDescriber.INVALID;
+
+		return IScaContentDescriber.VALID;
 	}
 
 	public IEditorInput getEditorInput(final Object contents) {
