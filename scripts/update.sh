2e09da26527d2119b6942f59d2ed29ff5d564222
Daniel Wille <daniel.wille@axiosengineering.com>
2016-06-23 16:27:51 -0400

    Ensure update script doesn't copy git ignore

--- scripts/update.sh
+++ scripts/update.sh
@@ -47,13 +47,18 @@ rm -rf ${rootdir}/plugins/omnijni/src/*
 rm -rf ${rootdir}/plugins/org.ossie/src/*
 
 cp -r ${bulkiodir}/libsrc/java/src/* ${rootdir}/plugins/bulkio/src
+
 cp -r ${bulkiodir}/src/java/* ${rootdir}/plugins/idl.bulkio.jni/src
 find ${rootdir}/plugins/idl.bulkio.jni/src -type f ! -path "*/jni/*" -delete
 find ${rootdir}/plugins/idl.bulkio.jni/src -mindepth 1 -type d -empty -delete
+
 cp -r ${cfdir}/src/base/framework/java/cf/src/* ${rootdir}/plugins/idl.cf.jni/src
 find ${rootdir}/plugins/idl.cf.jni/src -type f ! -path "*/jni/*" -delete
 find ${rootdir}/plugins/idl.cf.jni/src -mindepth 1 -type d -empty -delete
+
 cp -r ${cfdir}/src/omnijni/src/java/* ${rootdir}/plugins/omnijni/src
 find ${rootdir}/plugins/omnijni/src -type f ! -name "*.java" -delete
+
 cp -r ${cfdir}/src/base/framework/java/ossie/src/* ${rootdir}/plugins/org.ossie/src
+rm ${rootdir}/plugins/org.ossie/src/org/ossie/properties/.gitignore
 
