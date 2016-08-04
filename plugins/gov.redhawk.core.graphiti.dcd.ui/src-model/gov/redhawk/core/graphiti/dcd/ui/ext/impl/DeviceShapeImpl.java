6e19ac68cde76a4fab56def82fcfe655c384c16e
Daniel Wille <daniel.wille@axiosengineering.com>
2016-08-04 11:18:29 -0400

    Add SAD and DCD models

---
+++ plugins/gov.redhawk.core.graphiti.dcd.ui/src-model/gov/redhawk/core/graphiti/dcd/ui/ext/impl/DeviceShapeImpl.java
@@ -0,0 +1,49 @@
+/**
+ * This file is protected by Copyright.
+ * Please refer to the COPYRIGHT file distributed with this source distribution.
+ *
+ * This file is part of REDHAWK IDE.
+ *
+ * All rights reserved.  This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+// BEGIN GENERATED CODE
+package gov.redhawk.core.graphiti.dcd.ui.ext.impl;
+
+import gov.redhawk.core.graphiti.dcd.ui.ext.DeviceShape;
+import gov.redhawk.core.graphiti.dcd.ui.ext.RHDeviceGxPackage;
+
+import gov.redhawk.core.graphiti.ui.ext.impl.RHContainerShapeImpl;
+
+import org.eclipse.emf.ecore.EClass;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Device Shape</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * @generated
+ */
+public class DeviceShapeImpl extends RHContainerShapeImpl implements DeviceShape {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected DeviceShapeImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return RHDeviceGxPackage.Literals.DEVICE_SHAPE;
+	}
+
+} //DeviceShapeImpl
