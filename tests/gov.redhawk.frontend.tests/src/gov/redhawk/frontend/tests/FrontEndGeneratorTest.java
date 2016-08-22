80fd792f699dd236c1af0d26632fa5cc339fc2ce
Devin Nelson <devin.nelson@axiosengineering.com>
2016-08-22 14:30:53 -0400

    Test for IDE-1667

---
+++ tests/gov.redhawk.frontend.tests/src/gov/redhawk/frontend/tests/FrontEndGeneratorTest.java
@@ -0,0 +1,31 @@
+package gov.redhawk.frontend.tests;
+
+import org.eclipse.emf.common.util.BasicDiagnostic;
+import org.junit.Assert;
+import org.junit.Test;
+
+import gov.redhawk.frontend.util.TunerProperties;
+import mil.jpeojtrs.sca.prf.Struct;
+import mil.jpeojtrs.sca.prf.util.PrfValidator;
+
+public class FrontEndGeneratorTest {
+
+	/**
+	 * IDE-1667 - frontend_tuner_allocation should not be created with partially configured struct
+	 */
+	@Test
+	public void testGeneratedFeiDevicePropConfig() {
+		Struct[] structs = {
+			TunerProperties.TunerAllocationProperty.INSTANCE.createStruct(),
+			TunerProperties.ListenerAllocationProperty.INSTANCE.createStruct()
+		};
+		
+		for (Struct struct : structs) {
+			BasicDiagnostic diagnostics = new BasicDiagnostic();
+			PrfValidator.INSTANCE.validateStruct(struct, diagnostics, null);
+			if (!diagnostics.getChildren().isEmpty()) {
+				Assert.fail("Generated struct <" + struct.getId() + "> should not have configuration errors/warnings");
+			}
+		}
+	}
+}
