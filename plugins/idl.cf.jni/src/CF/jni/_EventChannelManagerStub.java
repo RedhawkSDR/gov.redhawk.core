ac09c7055c1169d8d7de7621c47d4c7ce4695e36
Daniel Wille <DWille@lgsinnovations.com>
2017-02-06 16:14:49 -0500

    IDE-1840 Add JNI code for EventChannelManager

--- plugins/idl.cf.jni/src/CF/jni/_EventChannelManagerStub.java
+++ plugins/idl.cf.jni/src/CF/jni/_EventChannelManagerStub.java
@@ -17,6 +17,12 @@ public class _EventChannelManagerStub extends omnijni.ObjectImpl implements CF.E
   }
   private static native org.omg.CosEventChannelAdmin.EventChannel create (long __ref__, String channel_name);
 
+  public org.omg.CosEventChannelAdmin.EventChannel get (String channel_name)
+  {
+    return get(this.ref_, channel_name);
+  }
+  private static native org.omg.CosEventChannelAdmin.EventChannel get (long __ref__, String channel_name);
+
   public org.omg.CosEventChannelAdmin.EventChannel createForRegistrations (String channel_name)
   {
     return createForRegistrations(this.ref_, channel_name);
@@ -34,6 +40,24 @@ public class _EventChannelManagerStub extends omnijni.ObjectImpl implements CF.E
     release(this.ref_, channel_name);
   }
   private static native void release (long __ref__, String channel_name);
+
+  public void forceRelease (String channel_name)
+  {
+    forceRelease(this.ref_, channel_name);
+  }
+  private static native void forceRelease (long __ref__, String channel_name);
+
+  public CF.EventChannelManagerPackage.EventChannelReg registerConsumer (org.omg.CosEventComm.PushConsumer consumer, CF.EventChannelManagerPackage.EventRegistration req)
+  {
+    return registerConsumer(this.ref_, consumer, req);
+  }
+  private static native CF.EventChannelManagerPackage.EventChannelReg registerConsumer (long __ref__, org.omg.CosEventComm.PushConsumer consumer, CF.EventChannelManagerPackage.EventRegistration req);
+
+  public CF.EventChannelManagerPackage.PublisherReg registerPublisher (CF.EventChannelManagerPackage.EventRegistration req, org.omg.CosEventComm.PushSupplier disconnectReceiver)
+  {
+    return registerPublisher(this.ref_, req, disconnectReceiver);
+  }
+  private static native CF.EventChannelManagerPackage.PublisherReg registerPublisher (long __ref__, CF.EventChannelManagerPackage.EventRegistration req, org.omg.CosEventComm.PushSupplier disconnectReceiver);
 
   public CF.EventChannelManagerPackage.EventChannelReg registerResource (CF.EventChannelManagerPackage.EventRegistration req)
   {
