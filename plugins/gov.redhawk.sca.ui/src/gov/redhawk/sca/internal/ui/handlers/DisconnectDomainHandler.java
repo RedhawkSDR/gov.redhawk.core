31f0bd8c9b627405e7cc99d0ef60d0b656f54d3f
Daniel Wille <DWille@lgsinnovations.com>
2017-11-15 17:45:46 -0500

    IDE-1545 Cleanup handlers; drop enablement code

--- plugins/gov.redhawk.sca.ui/src/gov/redhawk/sca/internal/ui/handlers/DisconnectDomainHandler.java
+++ plugins/gov.redhawk.sca.ui/src/gov/redhawk/sca/internal/ui/handlers/DisconnectDomainHandler.java
@@ -11,15 +11,12 @@
  */
 package gov.redhawk.sca.internal.ui.handlers;
 
-import gov.redhawk.model.sca.DomainConnectionState;
-import gov.redhawk.model.sca.ScaDomainManager;
-import gov.redhawk.sca.ui.ScaUiPlugin;
+import java.util.Iterator;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
-import org.eclipse.core.expressions.IEvaluationContext;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
@@ -28,72 +25,43 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 
-// TODO: Auto-generated Javadoc
-/**
- * The Class InitHandler.
- */
+import gov.redhawk.model.sca.ScaDomainManager;
+import gov.redhawk.sca.ui.ScaUiPlugin;
+
 public class DisconnectDomainHandler extends AbstractHandler implements IHandler {
 
 	@Override
-	public void setEnabled(final Object evaluationContext) {
-		if ((evaluationContext != null) && (evaluationContext instanceof IEvaluationContext)) {
-			final IEvaluationContext context = (IEvaluationContext) evaluationContext;
-			final Object sel = context.getVariable("selection");
-			if (sel instanceof IStructuredSelection) {
-				final IStructuredSelection ss = (IStructuredSelection) sel;
-				boolean enabled = true;
-				for (final Object obj : ss.toArray()) {
-					if (obj instanceof ScaDomainManager) {
-						final ScaDomainManager domMgr = (ScaDomainManager) obj;
-						if (!domMgr.getState().equals(DomainConnectionState.CONNECTED)) {
-							enabled = false;
-						}
-					}
-				}
-				this.setBaseEnabled(enabled);
-			} else {
-				super.setEnabled(evaluationContext);
-			}
-		}
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-
-	@Override
 	public Object execute(final ExecutionEvent event) throws ExecutionException {
-		ISelection selection = HandlerUtil.getActiveMenuSelection(event);
+		ISelection selection = HandlerUtil.getActiveMenuSelectionChecked(event);
 		if (selection == null) {
 			selection = HandlerUtil.getCurrentSelection(event);
 		}
-		if (selection instanceof IStructuredSelection) {
-			final IStructuredSelection ss = (IStructuredSelection) selection;
-			for (final Object obj : ss.toArray()) {
-				if (obj instanceof ScaDomainManager) {
-					final ScaDomainManager domMgr = (ScaDomainManager) obj;
-					final Job job = new Job("Disconnecting Domain") {
-
-						@Override
-						protected IStatus run(final IProgressMonitor monitor) {
-							monitor.beginTask("Disconnecting from domain " + domMgr.getLabel(), IProgressMonitor.UNKNOWN);
-							try {
-								domMgr.disconnect();
-								return Status.OK_STATUS;
-							} catch (final Exception e) { // SUPPRESS CHECKSTYLE Logged Catch all exception
-								return new Status(IStatus.ERROR, ScaUiPlugin.PLUGIN_ID, "Failed to connect", e);
-							}
-						}
-
-					};
-					job.setPriority(Job.LONG);
-					job.setUser(true);
-					job.schedule();
-				}
-			}
+		if (selection == null) {
+			return null;
 		}
-		// TODO Auto-generated method stub
+		IStructuredSelection ss = (IStructuredSelection) selection;
+
+		for (Iterator< ? > iter = ss.iterator(); iter.hasNext();) {
+			Object obj = iter.next();
+			if (!(obj instanceof ScaDomainManager)) {
+				continue;
+			}
+			final ScaDomainManager domMgr = (ScaDomainManager) obj;
+
+			Job job = Job.create("Disconnecting Domain", monitor -> {
+				monitor.beginTask("Disconnecting from domain " + domMgr.getLabel(), IProgressMonitor.UNKNOWN);
+				try {
+					domMgr.disconnect();
+					return Status.OK_STATUS;
+				} catch (final Exception e) { // SUPPRESS CHECKSTYLE Logged Catch all exception
+					return new Status(IStatus.ERROR, ScaUiPlugin.PLUGIN_ID, "Failed to connect", e);
+				}
+			});
+			job.setPriority(Job.LONG);
+			job.setUser(true);
+			job.schedule();
+		}
+
 		return null;
 	}
-
 }
