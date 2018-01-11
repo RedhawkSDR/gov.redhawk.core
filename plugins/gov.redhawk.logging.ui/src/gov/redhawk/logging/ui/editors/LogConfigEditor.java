d906bbec8f48f0659a046e5edc3f3f2503304d9d
Daniel Wille <DWille@lgsinnovations.com>
2018-01-11 12:32:05 -0500

    IDE-2151 Fix 3 potential resource leakage issues

--- plugins/gov.redhawk.logging.ui/src/gov/redhawk/logging/ui/editors/LogConfigEditor.java
+++ plugins/gov.redhawk.logging.ui/src/gov/redhawk/logging/ui/editors/LogConfigEditor.java
@@ -10,22 +10,26 @@
  *******************************************************************************/
 package gov.redhawk.logging.ui.editors;
 
-import java.io.File;
-import java.util.concurrent.Callable;
+import java.net.URI;
 
+import org.eclipse.core.filesystem.EFS;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.ILog;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.IURIEditorInput;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchListener;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.editors.text.TextEditor;
+import org.osgi.framework.FrameworkUtil;
 
 import CF.LogConfigurationOperations;
 import gov.redhawk.logging.ui.LoggingUiPlugin;
@@ -38,8 +42,6 @@ public class LogConfigEditor extends TextEditor {
 	public static final String ID = "gov.redhawk.logging.ui.logconfig.editor";
 
 	private LogConfigurationOperations resource;
-	private IPath path;
-
 	private IWorkbenchListener workBenchListener;
 
 	@Override
@@ -72,14 +74,12 @@ public class LogConfigEditor extends TextEditor {
 
 	@Override
 	public String getTitleToolTip() {
-		String toolTip = super.getTitleToolTip();
-
 		if (resource instanceof ScaAbstractComponent< ? >) {
-			toolTip = ((ScaAbstractComponent< ? >) resource).getIdentifier();
+			return ((ScaAbstractComponent< ? >) resource).getIdentifier();
 		} else if (resource instanceof ScaWaveform) {
-			toolTip = ((ScaWaveform) resource).getName();
+			return ((ScaWaveform) resource).getName();
 		}
-		return toolTip;
+		return super.getTitleToolTip();
 	}
 
 	@Override
@@ -90,29 +90,19 @@ public class LogConfigEditor extends TextEditor {
 
 	private void updateLogConfig() {
 		final String newLogConfig = getEditorText();
-		final Job saveLogConfigJob = new Job("Saving Log Configuration File...") {
-			@Override
-			protected IStatus run(IProgressMonitor monitor) {
-				monitor.beginTask(getName(), IProgressMonitor.UNKNOWN);
-				try {
-					CorbaUtils.invoke(new Callable<Object>() {
-						public Object call() throws Exception {
-							resource.setLogConfig(newLogConfig);
-							return Status.OK_STATUS;
-						};
-					}, monitor);
-				} catch (CoreException e) {
-					return new Status(e.getStatus().getSeverity(), LoggingUiPlugin.PLUGIN_ID, e.getLocalizedMessage(), e);
-				} catch (InterruptedException e) {
-					return Status.CANCEL_STATUS;
-				} finally {
-					monitor.done();
-				}
-
+		final Job saveLogConfigJob = Job.create("Saving log configuration file...", monitor -> {
+			try {
+				CorbaUtils.invoke(() -> {
+					resource.setLogConfig(newLogConfig);
+					return null;
+				}, monitor);
 				return Status.OK_STATUS;
+			} catch (CoreException e) {
+				return new Status(e.getStatus().getSeverity(), LoggingUiPlugin.PLUGIN_ID, e.getLocalizedMessage(), e);
+			} catch (InterruptedException e) {
+				return Status.CANCEL_STATUS;
 			}
-		};
-
+		});
 		saveLogConfigJob.setUser(true);
 		saveLogConfigJob.schedule();
 	}
@@ -121,27 +111,24 @@ public class LogConfigEditor extends TextEditor {
 		return this.getDocumentProvider().getDocument(this.getEditorInput()).get();
 	}
 
-	public LogConfigurationOperations getResource() {
-		return resource;
-	}
-
 	public void setResource(LogConfigurationOperations resource) {
 		this.resource = resource;
 		setTitleToolTip(getTitleToolTip());
 	}
 
-	public void setFilePath(IPath path) {
-		this.path = path;
-	}
-
 	@Override
 	public void dispose() {
 		super.dispose();
-		File file = path.toFile();
-		if (file.exists()) {
-			file.delete();
+		IEditorInput input = getEditorInput();
+		if (input instanceof IURIEditorInput) {
+			URI uri = ((IURIEditorInput) input).getURI();
+			try {
+				EFS.getStore(uri).delete(EFS.NONE, new NullProgressMonitor());
+			} catch (CoreException e) {
+				ILog log = Platform.getLog(FrameworkUtil.getBundle(getClass()));
+				log.log(new Status(IStatus.WARNING, LoggingUiPlugin.PLUGIN_ID, "Unable to delete temporary file: " + uri.toString(), e));
+			}
 		}
-
 		PlatformUI.getWorkbench().removeWorkbenchListener(this.workBenchListener);
 	}
 
