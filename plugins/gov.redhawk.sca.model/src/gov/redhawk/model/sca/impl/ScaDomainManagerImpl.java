65790d94b7c48bbe39c8487ff5823d4987fba5b5
Kevin Vig <kvig@geontech.com>
2019-05-20 11:41:08 -0400

    IDE-2298 - More debug

--- plugins/gov.redhawk.sca.model/src/gov/redhawk/model/sca/impl/ScaDomainManagerImpl.java
+++ plugins/gov.redhawk.sca.model/src/gov/redhawk/model/sca/impl/ScaDomainManagerImpl.java
@@ -2244,6 +2244,8 @@ public class ScaDomainManagerImpl extends ScaPropertyContainerImpl<DomainManager
 			System.out.println("KeepAlive found the object");
 			lostNarrowedObject = false;
 			doChildRefresh = true;
+		} else {
+			System.out.println("!!!!What happened?: " + tmpExists + " getObj(): " + (getObj() == null) + " lost: " + lostNarrowedObject);
 		}
 		if (shouldProceed != null && shouldProceed) {
 			try {
